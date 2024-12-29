d5218c1442694d3b9dfa97ec89767bb1f3f6cd94
Kooreis <kooreis.official@gmail.com>
2024-12-28 21:21:37 -0600

    Adds a while loop that iterates as long as both arrays have unprocessed elements. Inside the loop, it compares the current elements of the two arrays and adds the smaller one to arr3. It then increments the index of the array from which it took the element and the index of arr3.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,10 @@
-function mergeArrays(arr1, arr2) {
-    let i = 0;
-    let j = 0;
-    let k = 0;
-    let arr3 = [];
-}
+while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            arr3[k] = arr1[i];
+            i++;
+        } else {
+            arr3[k] = arr2[j];
+            j++;
+        }
+        k++;
+    }
